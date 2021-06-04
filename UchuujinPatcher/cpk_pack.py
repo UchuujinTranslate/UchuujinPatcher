b0173fe24a9c9ae5cf0800382cf4ed1deb1bc212
colebob9 <colebob9@gmail.com>
2021-06-03 22:09:10 -0500

    Implemented cpk_pack

--- UchuujinPatcher/cpk_pack.py
+++ UchuujinPatcher/cpk_pack.py
@@ -1,3 +1,32 @@
 # Pack modified sc scripts back into sc.cpk
 
 # Use CriPakTools here again.
+# for %%f in (*) do ..\..\executables\CriPakTools.exe ..\..\temp_pack\sc.cpk %%f ..\..\cpkdumps\sc\%%f && ECHO Put %%f file into "sc.cpk".
+import os
+from shutil import copyfile, rmtree
+
+pack_dir = "pack/"
+scripts = "weblate_scripts/"
+
+copyfile("bin/CriPakTools.exe", "isofiles/CriPakTools.exe")
+
+
+# Must execute in same dir as well
+cwd = os.getcwd()
+os.chdir("isofiles/")
+
+
+
+for filename in os.listdir(pack_dir):
+    # Call as python command right now, but later on,
+    # import as a module for better flexibility and speed,
+    # requires modifying main repo's code
+    print(f"Packing {filename}...")
+    os.system(f"CriPakTools.exe sc.cpk {filename} pack/{filename} new_sc.cpk")
+
+# Delete tool
+os.remove("CriPakTools.exe")
+
+# Go back to root dir
+# print(cwd)
+os.chdir(cwd)
--- UchuujinPatcher/patch_sc.py
+++ UchuujinPatcher/patch_sc.py
@@ -1,4 +1,6 @@
 # Call sc_patch_translations.py from main repo
+# Patches in English text into scripts
+
 # Multiprocess or implement looping through all in 
 # normal script
 # Implement it normal first, then add multiprocessing
@@ -9,10 +11,13 @@ pack_dir = "isofiles/pack/"
 scripts = "weblate_scripts/"
 
 for filename in os.listdir(pack_dir):
-    # Call as python script right now, but later on, 
+    # Call as python command right now, but later on, 
     # import as a module for better flexibility and speed, 
     # requires modifying main repo's code
     print(f"Patching {filename}...")
     os.system(
-        f"py master_src/sc/sc_patch_translations.py {pack_dir}{filename} {scripts}{filename}.json {scripts}en_US/{filename}.po")
+        f"py master_src/sc/sc_patch_translations.py \
+         {pack_dir}{filename} \
+         {scripts}{filename}.json \
+         {scripts}en_US/{filename}.po")
     
