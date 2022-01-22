1fc683f88f9808946aac65b594c4552da25b0592
gil-unx <57790408+gil-unx@users.noreply.github.com>
2022-01-22 17:04:01 +0800

    grab lt.bin

--- UchuujinPatcher/decompress_iso.py
+++ UchuujinPatcher/decompress_iso.py
@@ -21,5 +21,5 @@ iso.get_file_from_iso(
 iso.get_file_from_iso(
     "isofiles/EBOOT.BIN", iso_path='/PSP_GAME/SYSDIR/EBOOT.BIN')
 iso.get_file_from_iso(
-    "isofiles/lt.bin", iso_path='/PSP_GAME/USRDIR/lt.bin')
+    "isofiles/lt.bin", iso_path='/PSP_GAME/USRDIR/DATA/lt.bin')
 iso.close()
